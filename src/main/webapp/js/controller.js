af048fce971bfcc1ca4449f664e4db3e2cb6fbb2
Laci <ls@gerhardtinformatics.com>
2016-05-03 14:19:12 +0200

    add bbc2

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -261,7 +261,8 @@ controller = {
     	            {"file":"http://194.38.105.21:8002/", "name":"Jazz rádió"},
     	            {"file":"http://stream2.radio88.hu:8400/", "name":"Rádió88 Club 88"},
     	            {"file":"http://listen.trance.fm/1/192", "name":"trance.fm: [UK1] Trance Channe"},
-    	            {"file":"http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p", "name":"bbc.co.uk/radio1"},
+    	            {"file":"http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p", "name":"BBC radio 1"},
+    	            {"file":"http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio2_mf_p", "name":"BBC radio 2"},
     	            
     	            ];
     	controller.updateTable($("#radioTable"),list,"","stream")
