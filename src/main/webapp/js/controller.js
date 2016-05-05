df3d9faa093a7aa5d97cab646f2a120aa179eedf
Laci <ls@gerhardtinformatics.com>
2016-05-05 11:42:25 +0200

    download youtube to youtube folder

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -334,7 +334,7 @@ controller = {
         });
 	},
 	saveYoutubeLink: function(youtubeLink){
-   	    var command = "youtube-dl --extract-audio --audio-format mp3 -o '/home/pi/Music/%(title)s.%(ext)s' " + youtubeLink;
+   	    var command = "youtube-dl --extract-audio --audio-format mp3 -o '/home/pi/Music/youtube/%(title)s.%(ext)s' " + youtubeLink;
         $("#currentSongTtitle").html("Start downloading...");
         controller.sendCommand(command); 
 	},
