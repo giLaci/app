fad6cbc63d8e38c88bbc50e588a7baadfabf7326
Laci <ls@gerhardtinformatics.com>
2016-05-06 14:57:15 +0200

    fix

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -335,7 +335,7 @@ controller = {
 	},
 	saveYoutubeLink: function(youtubeLink){
    	    //var command = "youtube-dl --extract-audio --audio-format mp3 -o '/home/pi/Music/youtube/%(title)s.%(ext)s' " + youtubeLink;
-		var command = "/home/pi/yt_downloader.sh " + youtubeLink;
+		var command = "/home/pi/yt_download.sh " + youtubeLink;
         $("#currentSongTtitle").html("Start downloading...");
         controller.sendCommand(command); 
 	},
