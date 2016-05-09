934d60749aae1f94a3c29a298dac230d166eb887
Laci <ls@gerhardtinformatics.com>
2016-05-09 13:45:01 +0200

    tuning

--- src/main/java/com/laci/Commander.java
+++ src/main/java/com/laci/Commander.java
@@ -19,31 +19,41 @@ public class Commander {
     public String exec(String command) {
         String output = "";
         boolean result = true;
-        // init shell
-        ProcessBuilder builder = new ProcessBuilder("/bin/bash");
-        Process p = null;
         try {
-            p = builder.start();
-            BufferedWriter p_stdin = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
-
-            p_stdin.write(command);
-            p_stdin.newLine();
-            p_stdin.flush();
-            // downloadyoutube script don't need a final exit command, and don't wait for result
-            if (command.contains("downloadyoutube.sh")) {
-                output += "start downloading from youtube";
-            } else {
-                p_stdin.write("exit");
-                p_stdin.newLine();
-                p_stdin.flush();
-                // write stdout of shell (=output of all commands)
-                BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                String line = null;
-                while ((line = in.readLine()) != null) {
-                    output += line + "\n";
-                    System.out.println(line);
-                }
+//        	// init shell
+//        	ProcessBuilder builder = new ProcessBuilder("/bin/bash");
+//        	Process p = null;
+//            p = builder.start();
+//            BufferedWriter p_stdin = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+//
+//            p_stdin.write(command);
+//            p_stdin.newLine();
+//            p_stdin.flush();
+//            // downloadyoutube script don't need a final exit command, and don't wait for result
+//            if (command.contains("downloadyoutube.sh")) {
+//                output += "start downloading from youtube";
+//            } else {
+//                p_stdin.write("exit");
+//                p_stdin.newLine();
+//                p_stdin.flush();
+//                // write stdout of shell (=output of all commands)
+//                BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//                String line = null;
+//                while ((line = in.readLine()) != null) {
+//                    output += line + "\n";
+//                    System.out.println(line);
+//                }
+//            }
+        	Process proc = Runtime.getRuntime().exec(command);
+            java.io.InputStream is = proc.getInputStream();
+            java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+            if (s.hasNext()) {
+            	output += s.next();
             }
+            else {
+            	output += "";
+            }
+
         } catch (Exception e) {
             output += "Execution faiulre:" + e.toString();
             System.out.println(e);
