a65024b38530c9829c20fe960ff14e322539b4e0
Laci <ls@gerhardtinformatics.com>
2016-05-13 11:30:03 +0200

    fix2

--- src/main/java/com/laci/Commander.java
+++ src/main/java/com/laci/Commander.java
@@ -19,24 +19,23 @@ public class Commander {
     public String exec(String command) {
         String output = "";
         boolean result = true;
+        // init shell
+        ProcessBuilder builder = new ProcessBuilder("/bin/bash");
+        Process p = null;
         try {
-        	// init shell
-        	ProcessBuilder builder = new ProcessBuilder(command);
-        	Process p = null;
             p = builder.start();
-//            BufferedWriter p_stdin = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
-//
-//            p_stdin.write(command);
-//            p_stdin.newLine();
-//            p_stdin.flush();
-            final int processStatus = p.waitFor();
+            BufferedWriter p_stdin = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+
+            p_stdin.write(command);
+            p_stdin.newLine();
+            p_stdin.flush();
             // downloadyoutube script don't need a final exit command, and don't wait for result
             if (command.contains("downloadyoutube.sh")) {
                 output += "start downloading from youtube";
             } else {
-//                p_stdin.write("exit");
-//                p_stdin.newLine();
-//                p_stdin.flush();
+                p_stdin.write("exit");
+                p_stdin.newLine();
+                p_stdin.flush();
                 // write stdout of shell (=output of all commands)
                 BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
                 String line = null;
@@ -45,7 +44,6 @@ public class Commander {
                     System.out.println(line);
                 }
             }
-
         } catch (Exception e) {
             output += "Execution faiulre:" + e.toString();
             System.out.println(e);
